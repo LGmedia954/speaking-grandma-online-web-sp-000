e6f1af99e28ce1d34bb532ec1a7d83b8cba75a72
Leslie G <lesliegonzalez@me.com>
2020-06-27 16:13:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
- elsif phrase == "Hi Nana, how are you?"
-    return "HUH?! SPEAK UP, SONNY!"
- elsif phrase == "Hi!"
+ elsif phrase == "Hi Nana, how are you?" || phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
  elsif phrase == "WHAT DID YOU EAT TODAY?".upcase
     return "NO, NOT SINCE 1938!"
